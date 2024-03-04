9ab51d123d3750cd1191550a7f52f3b024b72065
majdhaidar <haidarmajd@hotmail.com>
2024-03-04 21:54:35 +0200

    in the application properties add the api host for the beer micro service. Create a beer client config class to prepare rest template object. create a beer client test to check and test find beer by id.

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BeerClient.java
@@ -0,0 +1,27 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+public class BeerClient {
+    @Setter
+    private String apiHost;
+    private final String BEER_URL_PATH = "/api/v1/beer/";
+    private final RestTemplate restTemplate;
+
+    public BeerClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID id) {
+        return restTemplate.getForObject(apiHost + BEER_URL_PATH + id.toString(), BeerDto.class);
+    }
+}
